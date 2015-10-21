59ef3f608370df9f0ae496c0be7088526e661a66
Sergei Voitenko <voitenko.sv@gmail.com>
2015-10-21 18:41:12 +0300

    MVC no DB

--- SpringMVCTutorial/src/main/java/com/mvc/test/UserController.java
+++ SpringMVCTutorial/src/main/java/com/mvc/test/UserController.java
@@ -1,18 +1,22 @@
 package com.mvc.test;
 
 import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Created by svoitenko on 21 Oct 2015.
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/list")
     public String getUsersList(Model model) throws JSONException {
-        model.addAttribute("message","Hello from controller!");
+        model.addAttribute("message", "Hello from controller!");
         return "userPage";
     }
 }
--- SpringMVCTutorial/src/main/webapp/pages/userPage.jsp
+++ SpringMVCTutorial/src/main/webapp/pages/userPage.jsp
@@ -1,3 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: svoitenko
+  Date: 21 Oct 2015
+  Time: 18:30
+  To change this template use File | Settings | File Templates.
+--%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
